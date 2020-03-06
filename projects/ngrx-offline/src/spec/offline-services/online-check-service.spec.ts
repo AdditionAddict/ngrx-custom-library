a70088c134f2a98370a57a2bdcc3acecd621661b
Andrew Allen <andrewa@Extruda.local>
2020-03-06 15:59:29 +0000

    tests: add NxaOnlineCheckService tests

--- projects/ngrx-offline/src/spec/offline-services/online-check-service.spec.ts
+++ projects/ngrx-offline/src/spec/offline-services/online-check-service.spec.ts
@@ -1,3 +1,52 @@
-describe('NxaOnlineCheckService', () => {
+import { NxaOnlineCheckService } from "../../lib/offline-services/online-check-service";
+import { of } from 'rxjs';
+
+fdescribe('NxaOnlineCheckService', () => {
     // TODO: add tests
+    let service: NxaOnlineCheckService
+
+    beforeAll(() => {
+        service = new NxaOnlineCheckService()
+    })
+
+    describe('#online$', () => {
+        it('should return true when window.navigator.onLine is true', (done: DoneFn) => {
+            spyOnProperty(window.navigator, 'onLine').and.returnValue(true);
+            service.online$().subscribe(isOnline => {
+                expect(isOnline).toBe(true)
+                done()
+            })
+        })
+
+        it('should return false when window.navigator.onLine is false', (done: DoneFn) => {
+            spyOnProperty(window.navigator, 'onLine').and.returnValue(false);
+            service.online$().subscribe(isOnline => {
+                expect(isOnline).toBe(false)
+                done()
+            })
+        })
+
+        it('should return true when offline$ is an observable of false', (done: DoneFn) => {
+            spyOnProperty(window.navigator, 'onLine').and.returnValue(true);
+            service.offline$ = of(false)
+
+            service.online$().subscribe(isOnline => {
+                expect(isOnline).toBe(true)
+                done()
+            })
+        })
+
+        it('should return false when offline$ is an observable of true', (done: DoneFn) => {
+            spyOnProperty(window.navigator, 'onLine').and.returnValue(true);
+            service.offline$ = of(true)
+
+            service.online$().subscribe(isOnline => {
+                expect(isOnline).toBe(false)
+                done()
+            })
+        })
+    })
+
+
+
 });
