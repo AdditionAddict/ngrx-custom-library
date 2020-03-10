c11106fb56d98137e35e6a8e7b7550f6a0f5e835
Andrew Allen <andrewa@Extruda.local>
2020-03-10 16:35:38 +0000

    demo: add mission service to missions-page.component

--- projects/ngrx-offline-demo/src/app/missions/containers/missions-page/missions-page.component.ts
+++ projects/ngrx-offline-demo/src/app/missions/containers/missions-page/missions-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Mission } from '../../../interfaces';
+import { MissionCollectionService } from '../../../ngrx-store/collection-services';
 
 @Component({
   selector: 'app-missions-page',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./missions-page.component.scss']
 })
 export class MissionsPageComponent implements OnInit {
+  loading$: Observable<boolean>;
+  missions$: Observable<Mission[]>;
 
-  constructor() { }
+  constructor(private missionService: MissionCollectionService) {
+    this.missions$ = missionService.entities$;
+    this.loading$ = missionService.loading$;
+  }
 
   ngOnInit() {
+    this.getMissions();
+  }
+
+  add(mission: Mission) {
+    this.missionService.add(mission);
+  }
+
+  delete(mission: Mission) {
+    this.missionService.delete(mission.uuid);
+  }
+
+  getMissions() {
+    this.missionService.getAll();
+  }
+
+  update(mission: Mission) {
+    this.missionService.update(mission);
   }
 
 }
