693e44ff396c3be08b57226e7166b093747aa34a
Andrew Allen <andrewa@Extruda.local>
2020-03-10 16:34:37 +0000

    demo: add store, devtools, effects and ngrx-offline to app.module

--- projects/ngrx-offline-demo/src/app/app.module.ts
+++ projects/ngrx-offline-demo/src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { NgrxStoreModule } from './ngrx-store/ngrx-store.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +17,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true,
+      }
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
+    NgrxStoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
